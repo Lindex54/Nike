7d750bfef7d1b7b3b419122613a74c7969b9084d
Malinde Godwin <malindegodwin9@gmail.com>
2025-06-27 12:51:27 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+} from "./sections";
+import Nav from "./componets/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="x1:padding-1 wide:padding-r padding-b"> Hero</section>
-    <section className="padding"> PopularPoducts</section>
-    <section className="padding"> SuperQuality</section>
-    <section className="padding"> Services</section>
-    <section className="padding"> Special Offer</section>
+    <Nav />
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x bg-black padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
