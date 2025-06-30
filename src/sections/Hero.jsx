36b662e95de75a5766f1693b11797612bb30523e
Malinde Godwin <malindegodwin9@gmail.com>
2025-06-30 10:19:20 +0300

    first commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../componets/Button";
 
 import { arrowRight } from "../assets/icons";
+
+import { statistics } from "../constants";
 const Hero = () => {
   return (
     <section
@@ -14,18 +16,57 @@ const Hero = () => {
       flex-col justify-center 
       items-start w-full max-xl:padding-x pt-28"
       >
-        <p> Our Summer Collections</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p
+          className="text-xl font-montserrat 
+        text-coral-red"
+        >
+          Our Summer Collections
+        </p>
+        <h1
+          className="mt-10 font-palanquin 
+        text-8xl max-sm:text-[72px] 
+        max-sm:leading-[82] font-bold"
+        >
+          <span
+            className="xl:bg-white 
+          xl:whitespace-nowrap relative z-10 pr-10 "
+          >
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
+          <span
+            className="text-coral-red
+           inline-block mt-3"
+          >
+            Nike
+          </span>{" "}
           Shoes
         </h1>
-        <p>
+        <p
+          className="font-montserrat 
+        text-slate-gray 
+        text:lg leading-8 mt-g mb-14 
+        sm:max-w-sm"
+        >
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
+
+        <div
+          className="flex justify-start 
+        items-start 
+        flex-wrap w-full mt-20 gap-16"
+        >
+          {statistics.map((stat, index) => (
+            <div key={stat.label}>
+              <p className="font-palanquin text-4xl font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
