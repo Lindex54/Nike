d2d9d752d740064937f8be30fba1eda1f649716b
Malinde Godwin <malindegodwin9@gmail.com>
2025-07-02 19:57:12 +0300

    ''

--- src/componets/PopularProductCard.jsx
+++ src/componets/PopularProductCard.jsx
@@ -1,12 +1,32 @@
 import { star } from "../assets/icons";
 
-const PopularProductCard = ({ imgURL, name, price }) => {
+const PopularProductCard = ({ imgURL, name, price, rating }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">
+          {rating}
+        </p>
       </div>
+      <h3
+        className="mt-2 leading-normal 
+      font-semibold 
+      font-palanquin 
+      text-2xl"
+      >
+        {name}
+      </h3>
+      <p
+        className="font-semibold 
+          text-2xl mt-2 
+          leading-normal 
+          font-montserrat 
+          text-coral-red"
+      >
+        {price}
+      </p>
     </div>
   );
 };
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,38 @@
+import Button from "../componets/Button";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between 
+      items-center max-lg:flex-col 
+      gap-10 w-full max-container"
+    >
+      <div
+        className="flex flex-1 
+      flex-col"
+      >
+        <h2
+          className="font-palanquin
+        text-4xl capitalize font-bold lg:max-w-lg"
+        >
+          We Provide You
+          <span className="text-coral-red"> Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p
+          className="font-montserrat
+        text-slate-gray
+        text:lg leading-8 mt-g mb-14
+        sm:max-w-sm"
+        >
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life
+        </p>
+        <Button label="View details" />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
