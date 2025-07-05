55dbe500c16e77d1368c323a6c638573a3e28388
Malinde Godwin <malindegodwin9@gmail.com>
2025-07-05 11:57:20 +0300

    first commit

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,10 +1,16 @@
+import { services } from "../constants";
+import ServiceCard from "../componets/ServiceCard";
 const Services = () => {
   return (
     <section
       className="max-container flex
   justify-center flex-wrap
   gap-9"
-    ></section>
+    >
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   );
 };
 
