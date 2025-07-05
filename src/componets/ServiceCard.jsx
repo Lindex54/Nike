349f71722c10ca7d67aea7464071b7463607abe8
Malinde Godwin <malindegodwin9@gmail.com>
2025-07-05 12:26:47 +0300

    first commit

--- src/componets/ServiceCard.jsx
+++ src/componets/ServiceCard.jsx
@@ -2,12 +2,14 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
     <div
       className="flex-1 sm:w-[350px]
-  sm:min-w-[]350px w-full rounded-[20px}
+  sm:min-w-[350px] w-full rounded-[20px}
   shadow-3xl px-10 py-16"
     >
-      <div className="W-11 h-11 flex justify-center items-center rounded-full bg-coral-red ">
+      <div className="w-11 h-11 flex justify-center items-center rounded-full bg-coral-red ">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
+      <h3 className="mt-5 font-bold leading-normal font-palanquin">{label}</h3>
+      <p>{subtext}</p>
     </div>
   );
 };
